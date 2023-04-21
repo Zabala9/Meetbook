3df94b8d9bea121c36071e8ee31534d28bc7ae32
Steven Zabala <zabalasteven9@gmail.com>
2023-04-21 14:57:48 -0700

    Working on session controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,4 @@
 class Api::UsersController < ApplicationController
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            login(@user)
-            render :show
-        else
-            render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
-        end
-    end
-
-    private
-    def user_params
-        params.require(:user).permit(:name, :lastname, :email, :password)
-    end
+  def create
+  end
 end
