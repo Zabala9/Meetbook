8ebd6902e085a969c0eab2cfb9e1818c2863d96e
Steven Zabala <zabalasteven9@gmail.com>
2023-04-21 14:29:22 -0700

    Finishing and testing validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,55 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  name            :string           not null
+#  lastname        :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    has_secure_password
+    
+    validates :name, presence: true, length: {in: 2..15},
+        format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+    validates :lastname, presence: true, length: {in: 2..15},
+        format: { without: URI::MailTo::EMAIL_REGEXP, message:  "can't be an email" }
+    validates :email, presence: true, uniqueness: true, length: {in: 5..255},
+        format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: {in: 6..255}, allow_nil: true
+
+    before_validation :ensure_session_token
+
+    def self.find_by_credential(email, password)
+        user = User.find_by(email: email)
+        if user&.authenticate(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def reset_session_token!
+        self.session_token = generate_session_token
+        self.save
+        self.session_token
+    end
+
+    private
+    def generate_session_token
+        token = SecureRandom::urlsafe_base64
+        while User.exists?(session_token: token)
+            token = SecureRandom::urlsafe_base64
+        end
+        token
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_session_token
+    end
 end
--- db/migrate/20230421184308_create_users.rb
+++ db/migrate/20230421184308_create_users.rb
@@ -1,8 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-
+      t.string :name, null: false
+      t.string :lastname, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.string :session_token, null: false
       t.timestamps
     end
+    add_index :users, [:name, :lastname]
+    add_index :users, [:email, :session_token], unique: true
   end
 end
