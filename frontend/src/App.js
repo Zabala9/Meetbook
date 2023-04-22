8d1e3caf6065ec8e5478a223d111153fc45df36e
Steven Zabala <zabalasteven9@gmail.com>
2023-04-22 15:44:15 -0700

    Adding functionality for log out

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,20 +2,24 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
 import SignupFormPage from "./components/SignupFormPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <Switch>
-      <Route path="/login" >
-        <LoginFormPage />
-      </Route>
-      <Route path="/signup" >
-        <SignupFormPage />
-      </Route>
-      <Route path="/" >
-        <h1>You are Login!</h1>
-      </Route>
-    </Switch>
+    <>
+      <Navigation />
+      <Switch>
+        <Route path="/login" >
+          <LoginFormPage />
+        </Route>
+        <Route path="/signup" >
+          <SignupFormPage />
+        </Route>
+        <Route path="/" >
+          <h1>You are Login!</h1>
+        </Route>
+      </Switch>
+    </>
   );
 }
 
