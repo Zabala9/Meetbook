a06ff5bf4e50d83d6b85edf125d2ad7639a41200
Steven Zabala <zabalasteven9@gmail.com>
2023-04-23 11:11:32 -0700

    creating  table, validation and routes for posts

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :string           not null
+#  author_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Post < ApplicationRecord
+    validates :content, :author_id, presence: true
+
+    belongs_to :user,
+    foreign_key: :author_id,
+    class_name: :User
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   post 'api/test', to: 'application#test'
 
   namespace :api, defaults: { format: :json } do
-    resources :users, only: :create
+    resources :users, only: :create do
+      resources :posts, only: [:index]
+    end
     resource :session, only: [:show, :create, :destroy]
+    resources :posts
   end
 end
