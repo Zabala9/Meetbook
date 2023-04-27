6fd55b7183fe941adc9ea3747c0d46fa79e4b700
Steven Zabala <zabalasteven9@gmail.com>
2023-04-27 10:03:41 -0700

    Adding style to the post show form

--- frontend/src/components/posts/PostShow.js
+++ frontend/src/components/posts/PostShow.js
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getPost, fetchPost } from "../../store/posts";
 import { useHistory, Redirect, useParams } from "react-router-dom";
 import PostsButton from "./PostButton";
+import './postShow.css';
 
 const PostShow = () => {
     const {postId} = useParams();
@@ -22,11 +23,16 @@ const PostShow = () => {
     if(!currentUser) return <Redirect to={'/'} />
     
     return(
-        <ul id="post-show">
-            <h3>{post.content}</h3>
-            <PostsButton key={post.id} post={post} />
-            <button onClick={goBack} >Go back</button>
-        </ul>
+        <div id="container-post-show">
+            <ul id="post-show">
+                <div id="container-content">
+                    <h3 id="content">{post.content}</h3>
+                    <label id="post-button"><PostsButton key={post.id} post={post} /></label>
+                </div>
+                <br></br>
+                <button onClick={goBack} id="go-back-button" >Go back</button>
+            </ul>
+        </div>
     )
 };
 
