c8fb97633cd69f701a3433778ec89106b7ff8ac7
Steven Zabala <zabalasteven9@gmail.com>
2023-05-23 01:55:11 -0700

    removing reload

--- frontend/src/components/Comments/CommentFormModal.js
+++ frontend/src/components/Comments/CommentFormModal.js
@@ -1,14 +1,19 @@
 import React, {useState} from "react";
 import { Modal } from "../../context/Modal";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import CommentForm from "./CommentForm";
 
 function CommentFormModal() {
     const history = useHistory();
+    const {postId} = useParams();
     const [showEditCommentModal, setShowEditCommentModal] = useState(true);
+
+    console.log(postId, 'here');
 
     const changeRoute = () => {
         history.goBack();
+        // let path = `/${postId}`;
+        // history.push(path);
     };
 
     return(
