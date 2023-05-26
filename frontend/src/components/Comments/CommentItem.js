d48d3f99a459973923921a8a975004693a465ab1
Steven Zabala <zabalasteven9@gmail.com>
2023-05-25 17:45:30 -0700

    Adding logic for commentButton

--- frontend/src/components/Comments/CommentItem.js
+++ frontend/src/components/Comments/CommentItem.js
@@ -1,16 +1,23 @@
 import React, {useEffect} from "react";
 import CommentButton from "./CommentButton";
 import { useSelector, useDispatch } from "react-redux";
+import { useParams } from 'react-router-dom';
 import { fetchUsers } from '../../store/user.js';
+import { getPost } from "../../store/posts";
 import './commentIndex.css';
 
 const CommentItem = ({comment}) => {
     const dispatch = useDispatch();
+    const {postId} = useParams();
     const currentUser = useSelector(state => state.session.user);
     const allUsers = useSelector(state => state.users);
+    const currentPost = useSelector(state => state.posts[postId]);
+
+    console.log(comment);
 
     useEffect(() => {
         dispatch(fetchUsers());
+        dispatch(getPost(postId));
     }, [dispatch]);
 
     let nameOwnerComment;
@@ -26,7 +33,9 @@ const CommentItem = ({comment}) => {
                 <label id="owner-comment">{currentUser.id === comment.authorId ? [currentUser.name + " " + currentUser.lastname] : nameOwnerComment}</label>
                 <div id="elements-li">
                     <label id="comment-content">{comment.content}</label>
-                    <CommentButton key={comment.id} comment={comment} />
+                    {currentPost.authorId === comment.authorId || 
+                        currentPost.authorId === currentUser.id && currentPost.id === comment.postId ||
+                        comment.authorId === currentUser.id ? <CommentButton key={comment.id} comment={comment} /> : undefined }
                 </div>
             </li>
         </div>
